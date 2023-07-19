2c1868373b1816a5a4f2a1891a799b4efd5818f0
Druiz45 <druiz45@itfip.edu.co>
2023-07-19 11:19:36 -0500

    Cambios reponse y vistas

--- views/public.php
+++ views/public.php
@@ -1,3 +1,6 @@
+<?php
+    $prueba = "pruebA";
+?>
 <body class="login">
     <div>
         <a class="hiddenanchor" id="signup"></a>
@@ -7,7 +10,7 @@
             <div class="animate form login_form">
                 <section class="login_content">
                     <form>
-                        <h1>Login Form</h1>
+                        <h1><?= $prueba ?></h1>
                         <div>
                             <input type="text" class="form-control" placeholder="Username" required="" />
                         </div>
@@ -76,3 +79,10 @@
         </div>
     </div>
 </body>
+<script>
+    const title = JSON.parse('<?= json_encode($prueba) ?>');
+    console.log(title);
+    window.addEventListener('DOMContentLoaded', () => {
+        document.getElementById('titulo-pagina').innerText = title;
+    })
+</script>
