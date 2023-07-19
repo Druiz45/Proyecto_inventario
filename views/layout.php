7e980e2532c13dee4ab54244eb3c385bc45c1494
Arrow0408 <mruiz27@itfip.edu.co>
2023-07-19 11:21:01 -0500

    New

--- views/layout.php
+++ views/layout.php
@@ -18,6 +18,15 @@
     <!-- Animate.css -->
     <link href="./assets/vendors/animate.css/animate.min.css" rel="stylesheet">
 
+    <!-- iCheck -->
+    <link href="./assets/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+    <!-- bootstrap-progressbar -->
+    <link href="./assets/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+    <!-- JQVMap -->
+    <link href="./assets/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet" />
+    <!-- bootstrap-daterangepicker -->
+    <link href="./assets/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+
     <!-- Custom Theme Style -->
     <link href="./assets/build/css/custom.min.css" rel="stylesheet">
   </head>
